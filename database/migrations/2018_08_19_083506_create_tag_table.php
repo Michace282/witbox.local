283e5d95f741a42fff9023a0c2f5107f6c0d2d0a
mishace282 <mishace282@gmail.com>
2022-01-20 21:14:52 +0200

    new version

--- database/migrations/2018_08_19_083506_create_tag_table.php
+++ database/migrations/2018_08_19_083506_create_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-/*class CreateTagTable extends Migration
+class CreateTagTable extends Migration
 {
 
     public function up()
@@ -26,4 +26,4 @@ use Illuminate\Database\Migrations\Migration;
     {
         Schema::dropIfExists('lts_tags');
     }
-}*/
+}
